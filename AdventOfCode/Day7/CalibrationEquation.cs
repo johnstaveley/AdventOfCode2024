6ccdc4ba2ccbf3e71e0fb2c871e86ff5a72944a0
John Staveley <john.staveley@myglobalbridge.com>
2024-12-07 15:26:26 +0000

    Next punt, not getting very far

--- AdventOfCode/Day7/CalibrationEquation.cs
+++ AdventOfCode/Day7/CalibrationEquation.cs
@@ -3,10 +3,28 @@
     public class CalibrationEquation
     {
         public Int64 Total { get; set; } = 0;
-        public List<Int64> Terms {  get; set; } = [];
+        public List<Int64> Terms { get; set; } = [];
+        public List<string> Operators { get; set; } = [];
         public bool CanBeComputed()
         {
-            return Terms.Sum() == Total;
+            var possibleOperators = new string[] { "+", "*" };
+            Operators = Terms.Select(a => "+").ToList();
+            foreach (var op in possibleOperators)
+            {
+                var operatorIndex = 0;
+                var total = Terms.First();
+                Operators[operatorIndex] = op;
+                foreach (var term in Terms.Skip(1))
+                {
+                    total = Operators[operatorIndex] == "+" ? total + term : total * term;
+                }
+                if (total == Total)
+                {
+                    return true;
+                }
+                operatorIndex++;
+            }
+            return false;
         }
     }
 }
--- AdventOfCode/Day7/Day7.cs
+++ AdventOfCode/Day7/Day7.cs
@@ -4,7 +4,7 @@ public static class Day7
 {
     public static void Execute()
     {
-        string filePath = "Day7/Input.txt";
+        string filePath = "Day7/Test.txt";
         try
         {
             string[] lines = File.ReadAllLines(filePath);
@@ -14,7 +14,7 @@ public static class Day7
             foreach (var line in lines)
             {
                 var equation = new CalibrationEquation();
-                 var terms = line.Split(':');
+                var terms = line.Split(':');
                 equation.Total = Int64.Parse(terms[0]);
                 equation.Terms = terms[1].Split(' ').Where(a => !string.IsNullOrEmpty(a)).Select(b => Int64.Parse(b.Trim(' '))).ToList();
                 equations.Add(equation);
@@ -24,7 +24,7 @@ public static class Day7
             {
                 Console.WriteLine($"Equation: {equation.Total} = {equation.CanBeComputed()}");
             }
-            Console.WriteLine($"Total Equations which can be calculated: {totalEquationsCalculated}");
+            Console.WriteLine($"Total Equations which can be calculated: {totalEquationsCalculated} out of {equations.Count}");
         }
         catch (Exception ex)
         {
