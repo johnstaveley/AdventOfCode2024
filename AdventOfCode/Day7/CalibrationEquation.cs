53356ec0ccdc277abc3037a2883c97cd17dd32d6
John Staveley <john.staveley@myglobalbridge.com>
2024-12-07 12:41:16 +0000

    basic day 7 computation

---
+++ AdventOfCode/Day7/CalibrationEquation.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode.Day7
+{
+    public class CalibrationEquation
+    {
+        public Int64 Total { get; set; } = 0;
+        public List<Int64> Terms {  get; set; } = [];
+        public bool CanBeComputed()
+        {
+            return Terms.Sum() == Total;
+        }
+    }
+}
--- AdventOfCode/Day7/Day7.cs
+++ AdventOfCode/Day7/Day7.cs
@@ -1,4 +1,6 @@
-﻿public static class Day7
+﻿using AdventOfCode.Day7;
+
+public static class Day7
 {
     public static void Execute()
     {
@@ -7,7 +9,22 @@
         {
             string[] lines = File.ReadAllLines(filePath);
             Console.WriteLine($"Read {lines.Length} lines from {filePath}");
-
+            var equations = new List<CalibrationEquation>();
+            var totalEquationsCalculated = 0;
+            foreach (var line in lines)
+            {
+                var equation = new CalibrationEquation();
+                 var terms = line.Split(':');
+                equation.Total = Int64.Parse(terms[0]);
+                equation.Terms = terms[1].Split(' ').Where(a => !string.IsNullOrEmpty(a)).Select(b => Int64.Parse(b.Trim(' '))).ToList();
+                equations.Add(equation);
+                totalEquationsCalculated += equation.CanBeComputed() ? 1 : 0;
+            }
+            foreach (var equation in equations)
+            {
+                Console.WriteLine($"Equation: {equation.Total} = {equation.CanBeComputed()}");
+            }
+            Console.WriteLine($"Total Equations which can be calculated: {totalEquationsCalculated}");
         }
         catch (Exception ex)
         {
