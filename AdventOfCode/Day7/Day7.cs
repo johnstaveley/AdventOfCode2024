3416ba69bf43c4d661210059135f68735f30dd20
John Staveley <john.staveley@myglobalbridge.com>
2024-12-07 16:23:53 +0000

    Created stub for day 8

--- AdventOfCode/Day7/Day7.cs
+++ AdventOfCode/Day7/Day7.cs
@@ -11,6 +11,7 @@ public static class Day7
             Console.WriteLine($"Read {lines.Length} lines from {filePath}");
             var equations = new List<CalibrationEquation>();
             var totalEquationsCalculated = 0;
+            Int64 totalCalibrationResult = 0;
             foreach (var line in lines)
             {
                 var equation = new CalibrationEquation();
@@ -18,13 +19,19 @@ public static class Day7
                 equation.Total = Int64.Parse(terms[0]);
                 equation.Terms = terms[1].Split(' ').Where(a => !string.IsNullOrEmpty(a)).Select(b => Int64.Parse(b.Trim(' '))).ToList();
                 equations.Add(equation);
-                totalEquationsCalculated += equation.CanBeComputed() ? 1 : 0;
+                var canBeComputed = equation.CanBeComputed();
+                if (canBeComputed)
+                {
+                    totalEquationsCalculated += 1;
+                    totalCalibrationResult += equation.Total;
+                }
+
             }
             foreach (var equation in equations)
             {
                 Console.WriteLine($"Equation: {equation.Total} = {equation.CanBeComputed()}");
             }
-            Console.WriteLine($"Total Equations which can be calculated: {totalEquationsCalculated} out of {equations.Count}");
+            Console.WriteLine($"Total Equations which can be calculated: {totalEquationsCalculated} out of {equations.Count} with calibration result: {totalCalibrationResult}");
         }
         catch (Exception ex)
         {
---
+++ AdventOfCode/Day8/Day8.cs
@@ -0,0 +1,17 @@
+﻿public static class Day8
+{
+    public static void Execute()
+    {
+        string filePath = "Day8/Input.txt";
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            Console.WriteLine($"Read {lines.Length} lines from {filePath}");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
