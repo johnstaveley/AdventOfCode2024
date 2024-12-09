4a2ef7612eee559f1c2fdb033f20c37494cc1b94
John Staveley <john@staveley.org>
2024-12-09 05:41:13 +0000

    Day 9 part 1

--- AdventOfCode/Day09/Day09.cs
+++ AdventOfCode/Day09/Day09.cs
@@ -1,4 +1,6 @@
-﻿public static class Day09
+﻿using AdventOfCode.Utility;
+
+public static class Day09
 {
     public static void Execute()
     {
@@ -7,7 +9,39 @@
         {
             string[] lines = File.ReadAllLines(filePath);
             Console.WriteLine($"Read {lines.Length} lines from {filePath}");
-
+            var diskmap = lines[0].ToCharArray().Select(a => int.Parse(a.ToString())).ToList();
+            var currentId = 0;
+            var isBlock = true;
+            var blocks = new List<int?>();
+            foreach(var item in diskmap)
+            {
+                if (isBlock)
+                {
+                    blocks.AddRange(Enumerable.Repeat((int?) currentId, item));
+                    currentId++;
+                }
+                else
+                {
+                    blocks.AddRange(Enumerable.Repeat((int?) null, item));
+                }
+                //if (currentId == 10) currentId = 0;
+                isBlock = !isBlock;
+            }
+            //PrintBlocks(blocks);
+            for (int i = blocks.Count - 1; i >= 0; i--)
+            {
+                var currentBlock = blocks[i];
+                if (currentBlock.HasValue)
+                {
+                    var locationOfFirstNull = blocks.IndexOf(null);
+                    if (locationOfFirstNull < i) {
+                        blocks.Swap(i, locationOfFirstNull);
+                        //PrintBlocks(blocks);
+                    }
+                }
+            }
+            //PrintBlocks(blocks);
+            Console.WriteLine($"Checksum: {GetChecksum(blocks)}");
         }
         catch (Exception ex)
         {
@@ -15,4 +49,24 @@
         }
     }
 
+    private static void PrintBlocks(List<int?> blocks)
+    {
+        foreach (var block in blocks)
+        {
+            Console.Write(block?.ToString() ?? ".");
+        }
+        Console.WriteLine();
+    }
+    private static Int64 GetChecksum(List<int?> blocks)
+    {
+        Int64 checkSum = 0;
+        for (int i = 0; i < blocks.Count; i++)
+        {
+            if (blocks[i].HasValue)
+            {
+                checkSum += i * blocks[i].Value;
+            }
+        }
+        return checkSum;
+    }
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine();
 Console.WriteLine("Advent of code");
 Console.WriteLine();
-Day07.Execute();
+Day09.Execute();
 Console.WriteLine();
 Console.WriteLine("Press key to end");
 Console.WriteLine();
