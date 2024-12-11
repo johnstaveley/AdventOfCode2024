8727d48aab39376139f97f0acbac88e8813f1450
John Staveley <john@staveley.org>
2024-12-11 07:10:52 +0000

    Base Day 11

---
+++ AdventOfCode/Day11/Day11.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode.Day10;
+using AdventOfCode.Model;
+
+public static class Day11
+{
+    public static void Execute()
+    {
+        string filePath = "Day11/Input.txt";
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            Console.WriteLine($"Read {lines.Length} lines from {filePath}");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+   
+}
