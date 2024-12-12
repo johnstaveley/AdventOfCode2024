a1db3272a8b26c65b2a67528a7cd082a34b1f90e
John Staveley <john@staveley.org>
2024-12-12 05:41:43 +0000

    Day 12 initial punt

--- AdventOfCode/Day12/Day12.cs
+++ AdventOfCode/Day12/Day12.cs
@@ -1,4 +1,9 @@
-﻿public static class Day12
+﻿using AdventOfCode.Day10;
+using AdventOfCode.Day12;
+using AdventOfCode.Model;
+using System.IO;
+
+public static class Day12
 {
     public static void Execute()
     {
@@ -7,11 +12,55 @@
         {
             string[] lines = File.ReadAllLines(filePath);
             Console.WriteLine($"Read {lines.Length} lines from {filePath}");
+            var map = new Map(lines);
+            var regions = GetRegions(map);
+            Console.WriteLine($"There are {regions.Count} regions");
         }
         catch (Exception ex)
         {
             Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
-
+    public static List<Region> GetRegions(Map map)
+    {
+        var regions = new List<Region>();
+        for (int i = 0; i < map.Grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.Grid.GetLength(1); j++)
+            {
+                if (map.Results[i, j] == "")
+                {
+                    var region = new Region()
+                    {
+                        FlowerType = map.Grid[i, j]
+                    };
+                    GetRegion(map, i, j, region);
+                    regions.Add(region);
+                }
+            }
+        }
+        return regions;
+    }
+    public static void GetRegion(Map map, int i, int j, Region region)
+    {
+        var searchLocations = new List<Location>
+        {
+            new Location { X = i - 1, Y = j },
+            new Location { X = i + 1, Y = j },
+            new Location { X = i, Y = j - 1 },
+            new Location { X = i, Y = j + 1 }
+        };
+        map.Results[i, j] = "#";
+        var searchCharacter = map.Grid[i, j];
+        foreach (var searchLocation in searchLocations)
+        {
+            if (map.IsOffGrid(searchLocation.X, searchLocation.Y) || 
+                map.Grid[searchLocation.X, searchLocation.Y] != searchCharacter || map.Results[searchLocation.X, searchLocation.Y] != "")
+            {
+                return;
+            }
+            region.Points.Add((searchLocation.X, searchLocation.Y));
+            GetRegion(map, searchLocation.X, searchLocation.Y, region);
+        }
+    }
 }
---
+++ AdventOfCode/Day12/Region.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode.Day12
+{
+    public class Region
+    {
+        public List<(int, int)> Points { get; set; }
+        public string FlowerType { get; set; } = "";
+        public Region()
+        {
+            Points = new List<(int, int)>();
+        }
+    }
+}
