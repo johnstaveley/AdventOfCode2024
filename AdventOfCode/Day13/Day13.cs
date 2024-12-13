c36e05d007c40c8960747bb75d59201e907f926b
John Staveley <john@staveley.org>
2024-12-13 05:19:16 +0000

    Stub day 13

---
+++ AdventOfCode/Day13/Day13.cs
@@ -0,0 +1,17 @@
+﻿public static class Day13
+{
+    public static void Execute()
+    {
+        string filePath = "Day13/Input.txt";
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            Console.WriteLine($"Read {lines.Length} lines from {filePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+
+}
