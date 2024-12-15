2204367e1f4a6b22d1573904d70cd4aff47c3038
John Staveley <john@staveley.org>
2024-12-15 07:00:52 +0000

    Stub for day 16

---
+++ AdventOfCode/Day16/Day16.cs
@@ -0,0 +1,17 @@
+﻿public static class Day16
+{
+    public static void Execute()
+    {
+        string filePath = "Day16/input.txt";
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            Console.WriteLine($"Read {lines.Length} lines from {filePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+   
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine();
 Console.WriteLine("Advent of code");
 Console.WriteLine();
-Day15.Execute();
+Day16.Execute();
 Console.WriteLine();
 Console.WriteLine("Press key to end");
 Console.WriteLine();
