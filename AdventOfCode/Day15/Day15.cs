a9f4c8fb28a121d8ee511a2866501ac85d1e9e25
John Staveley <john@staveley.org>
2024-12-15 06:20:53 +0000

    Move dev

--- AdventOfCode/Day15/Day15.cs
+++ AdventOfCode/Day15/Day15.cs
@@ -1,12 +1,21 @@
-﻿public static class Day15
+﻿using AdventOfCode.Model;
+
+public static class Day15
 {
     public static void Execute()
     {
-        string filePath = "Day15/Input.txt";
+        string filePath = "Day15/Test.txt";
         try
         {
             string[] lines = File.ReadAllLines(filePath);
             Console.WriteLine($"Read {lines.Length} lines from {filePath}");
+            var emptyLine = lines.First(a => string.IsNullOrWhiteSpace(a));
+            var mapLines = lines.TakeWhile(a => !string.IsNullOrEmpty(a)).ToArray();
+            var map = new Map(mapLines);
+            map.Display();
+            var movementLines = string.Join("", lines.Where(a => a.Contains("<") || a.Contains(">")).ToArray());
+            var movements = movementLines.ToArray().ToList();
+            Console.WriteLine(movements.Count);
         }
         catch (Exception ex)
         {
