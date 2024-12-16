9e6bad1459954ac1ac3c892aebb8a2b34ca4d716
John Staveley <john.staveley@myglobalbridge.com>
2024-12-16 08:11:52 +0000

    Minor refactor

--- AdventOfCode/Day16/Day16.cs
+++ AdventOfCode/Day16/Day16.cs
@@ -1,4 +1,5 @@
-﻿using AdventOfCode.Model;
+﻿using AdventOfCode.Day16;
+using AdventOfCode.Model;
 
 public static class Day16
 {
@@ -15,11 +16,11 @@ public static class Day16
             var startLocation = map.FindFirst("S");
             Console.WriteLine($"Robot is at {startLocation.Item1+1}:{startLocation.Item2+1}");
             map.Results[startLocation.Item1, startLocation.Item2] = "00";
-            var pathTravelled = new List<(int, int)>()
+            var pathTravelled = new List<Location>()
             {
-                (startLocation.Item1, startLocation.Item2)
+                new Location { X = startLocation.Item1, Y = startLocation.Item2 }
             };
-            FindPaths(map, startLocation, 1, 0, DirectionEnum.East, pathTravelled);
+            FindPaths(map, startLocation, 0, 0, DirectionEnum.East, pathTravelled);
             map.DisplayResults("** ");
         }
         catch (Exception ex)
@@ -27,25 +28,28 @@ public static class Day16
             Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
-    public static void FindPaths(Map map, (int, int) location, int currentDistance, int numberOfTurns, DirectionEnum currentlyFacing, 
-        List<(int, int)> pathTravelled)
+    public static void FindPaths(Map map, (int, int) location, int numberOfSteps, int numberOfTurns, DirectionEnum currentlyFacing,
+        List<Location> pathTravelled)
     {
         map.GetSearchLocations(location.Item1, location.Item2).ForEach(l =>
         {
             if (map.Grid[l.X, l.Y] == "#" || map.Grid[l.X, l.Y] == "S")
             {
+                // Hit a wall or the start location
                 return;
             }
-            if (pathTravelled.Contains((l.X, l.Y)))
+            if (pathTravelled.Any(a => a.X == l.X && a.Y == l.Y))
             {
+                // Already been here
                 return;
             }
-            map.Results[l.X, l.Y] = currentDistance.ToString();
-            pathTravelled.Add((l.X, l.Y));
+            map.Results[l.X, l.Y] = numberOfSteps.ToString();
+            pathTravelled.Add(new Location { X = l.X, Y = l.Y });
+            numberOfSteps++;
             if (map.Grid[l.X, l.Y] == "E")
             {
-                var finalScore = numberOfTurns * 1000 + currentDistance;
-                Console.WriteLine($"Exit found at {l.X + 1}:{l.Y + 1} after {currentDistance} steps {numberOfTurns} turns and with a score of {finalScore}");
+                var finalScore = numberOfTurns * 1000 + numberOfSteps;
+                Console.WriteLine($"Exit found at {l.X + 1}:{l.Y + 1} after {numberOfSteps} steps {numberOfTurns} turns and with a score of {finalScore}");
                 return;
             }
             var nextDirection = DirectionEnum.North;
@@ -63,16 +67,16 @@ public static class Day16
             }
             if (l.X > location.Item1)
             {
-                nextDirection = DirectionEnum.East;
+                nextDirection = DirectionEnum.East; 
             }
             if (currentlyFacing != nextDirection)
             {
-                numberOfTurns ++;
+                numberOfTurns++;
                 currentlyFacing = nextDirection;
-                Console.WriteLine($"Turned to face {nextDirection} at {l.X}:{l.Y} after {currentDistance} steps {numberOfTurns} turns");
+                //Console.WriteLine($"Turned to face {nextDirection} at {l.X}:{l.Y} after {currentDistance} steps {numberOfTurns} turns");
             }
-            var newPathTravelled = new List<(int, int)>(pathTravelled);
-            FindPaths(map, (l.X, l.Y), currentDistance + 1, numberOfTurns, currentlyFacing, newPathTravelled);
+            var newPathTravelled = new List<Location>(pathTravelled).ToList();
+            FindPaths(map, (l.X, l.Y), numberOfSteps, numberOfTurns, currentlyFacing, newPathTravelled);
         });
     }
 }
---
+++ AdventOfCode/Day16/Location.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode.Day16
+{
+    public class Location
+    {
+        public Int64 X { get; set; }
+        public Int64 Y { get; set; }
+    }
+}
